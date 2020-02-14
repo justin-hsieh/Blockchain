c45e814b7c1a14c4eaf8ae832bf636d892fad74c
Justin <showin@live.com>
2020-02-13 23:08:29 -0700

    add wallet

---
+++ basic_wallet_p/app.py
@@ -0,0 +1,150 @@
+import hashlib
+import requests
+import sys
+import json
+import time
+
+
+def proof_of_work(block):
+    """
+    Simple Proof of Work Algorithm
+    Stringify the block and look for a proof.
+    Loop through possibilities, checking each one against `valid_proof`
+    in an effort to find a number that is a valid proof
+    :return: A valid proof for the provided block
+    """
+    start = time.time()
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    end = time.time()
+    timer = end - start
+    print(f'Runtime: {timer} seconds')
+    return proof
+
+
+def valid_proof(block_string, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 6
+    leading zeroes?  Return true if the proof is valid
+    :param block_string: <string> The stringified block to use to
+    check in combination with `proof`
+    :param proof: <int?> The value that when combined with the
+    stringified previous block results in a hash that has the
+    correct number of leading zeroes.
+    :return: True if the resulting hash is a valid proof, False otherwise
+    """
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+        
+    return guess_hash[:3] == "000"
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+        self.transactions = []
+
+    def change_name(self, new_name):
+        self.name = new_name
+        return f'\nYou have changed your name to {self.name}\n'
+
+    def get_balance(self):
+        return f"\n{self.name} has {self.balance} coin(s).\n"
+
+    def get_transactions(self):
+        return f"\n{self.name} transaction's: {self.transactions}\n"
+
+
+if __name__ == '__main__':
+    check_name = True
+    while check_name == True:
+        name = input("Enter your name: ")
+        confirm_name = input(f'Confirm {name} as your name? press y')
+        if (confirm_name == 'y'):
+            newUser = User(name)
+            check_name = False
+
+    # Prints a message to confirm creation of new user
+    print(f"\nNew user created for {newUser.name}!\n")
+    # Asks the user what it would like to do
+    next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+    # Loops forever until user enters q
+    while True:
+        if (next_input == '1'):
+            new_name = str(input('\nWhat would you like to change your name to?\n'))
+            print(newUser.change_name(new_name))
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+
+        elif (next_input == '2'):
+            print(newUser.get_balance())
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+
+        elif (next_input == '3'):
+            print(newUser.get_transactions())
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+
+        elif (next_input == '4'):
+            node = "http://127.0.0.1:5000"
+            recip = input("\nWho is the recipient?\n")
+            amount = input("\nHow many coins do you want to send?\n")
+
+            while (int(amount) > newUser.balance):
+                print("\nYou don't have enough coins.\n")
+                amount = input("\nHow many coins do you want to send?\n")
+
+            post_data = {"sender": newUser.name, "recipient": recip, "amount": amount}
+            r = requests.post(url=node + '/transaction/new', json=post_data)
+            data = r.json()
+            print('\n' + data['message'] + '\n')
+            newUser.balance -= int(amount)
+            newUser.transactions.append({'sender': newUser.name, 'recipient': recip, 'amount': int(amount), 'index': data['message']})
+
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+
+        elif (next_input == '5'):
+            node = "http://127.0.0.1:5000"
+            coins_mined = 0
+            while True:
+                r = requests.get(url=node + "/last_block")
+                # Handle non-json response
+                try:
+                    data = r.json()
+                except ValueError:
+                    print("Error:  Non-json response")
+                    print("Response returned:")
+                    print(r)
+                    break
+
+                # TODO: Get the block from `data` and use it to look for a new proof
+                new_proof = proof_of_work(data['last_block'])
+
+                # When found, POST it to the server {"proof": new_proof, "id": username}
+                print(f'Id is {newUser.name}')
+                post_data = {"proof": new_proof, "id": newUser.name}
+
+                r = requests.post(url=node + "/mine", json=post_data)
+                data = r.json()
+                print(data['message'])
+                # TODO: If the server responds with a 'message' 'success'
+                # add 1 to the number of coins mined and print it.  Otherwise,
+                # print the message from the server.
+                if (data['message'].lower() == 'success'):
+                    newUser.balance += 1
+                    coins_mined += 1
+                    print(f'You have mined {coins_mined} coin(s). This round.')
+                else:
+                    print(data['message'])
+                stop = input("\n Would you like to stop mining?(y/n)\n")
+                if (stop.lower() == 'y'):
+                    break
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
+
+        elif (next_input.lower() == 'q'):
+            break
+
+        else:
+            print('Input not recognized. Try again...')
+            next_input = str(input("What would you like to do:\nChange name(1)\nView balance(2)\nView transactions(3)\nMake transaction(4)\nMine for coin(5)\nQuit(q)?"))
